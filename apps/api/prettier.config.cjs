c959c2b3edba270f0f411534a0d5e48744c1a1aa
bflorestal <yayou7710@hotmail.fr>
2024-02-28 22:32:34 +0100

    Création de l'API NestJS

---
+++ apps/api/prettier.config.cjs
@@ -0,0 +1,13 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+
+/** @type { PrettierConfig | SortImportsConfig } */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+};
+
+module.exports = config;
