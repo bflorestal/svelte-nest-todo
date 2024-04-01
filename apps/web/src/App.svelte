2adfb596b8d0d067f7f3b5f1dfe23ecabcf0236d
bflorestal <yayou7710@hotmail.fr>
2024-04-02 00:56:29 +0200

    Ajout du composant principal

--- apps/web/src/App.svelte
+++ apps/web/src/App.svelte
@@ -1,7 +1,64 @@
 <script lang="ts">
-  console.log("Hello world!");
+  import { Button } from "$lib/components/ui/button";
+  import { Input } from "$lib/components/ui/input";
+  import { Checkbox } from "$lib/components/ui/checkbox";
+  import {
+    CardTitle,
+    CardDescription,
+    CardHeader,
+    CardContent,
+    Card,
+  } from "$lib/components/ui/card";
+  import { FileEditIcon, PlusIcon, TrashIcon } from "lucide-svelte";
+
+  const EXAMPLE_TASKS = [
+    "Acheter des provisions pour la semaine",
+    "Appeler maman",
+    "Terminer le rapport",
+  ];
 </script>
 
-<main class="">
-  <h1>Vite + Svelte</h1>
+<main class="grid">
+  <Card class="w-full max-w-lg mx-auto">
+    <CardHeader>
+      <CardTitle tag="h1" class="text-lg">To-do list</CardTitle>
+      <CardDescription>Gérez vos tâches</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <div class="flex items-center gap-2">
+        <Input
+          class="flex-1 min-w-0"
+          placeholder="Ajouter une tâche"
+          type="text"
+        />
+        <Button class="h-8 w-8" size="icon" variant="outline">
+          <PlusIcon class="h-4 w-4" />
+          <span class="sr-only">Ajouter</span>
+        </Button>
+      </div>
+      {#each EXAMPLE_TASKS as task, idx}
+        <div class="grid gap-2 mt-4">
+          <div class="flex items-center gap-4">
+            <Checkbox class="peer-hidden" id={`task${idx}`} />
+            <label
+              class="flex-1 text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              for="task1"
+            >
+              {task}
+            </label>
+            <div class="flex gap-2 ml-auto">
+              <Button class="h-6 w-6" size="icon" variant="outline">
+                <FileEditIcon class="h-4 w-4" />
+                <span class="sr-only">Modifier</span>
+              </Button>
+              <Button class="h-6 w-6" size="icon" variant="outline">
+                <TrashIcon class="h-4 w-4" />
+                <span class="sr-only">Supprimer</span>
+              </Button>
+            </div>
+          </div>
+        </div>
+      {/each}
+    </CardContent>
+  </Card>
 </main>
