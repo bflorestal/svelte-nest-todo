ef8dc4bd14eaa6c8ad434c073a91663de22a292c
bflorestal <yayou7710@hotmail.fr>
2024-04-03 01:13:30 +0200

    Titre des tâches limité à 50 caractères

--- apps/api/src/todo/dto/create-todo.dto.ts
+++ apps/api/src/todo/dto/create-todo.dto.ts
@@ -1,10 +1,17 @@
-import { IsBoolean, IsNotEmpty, IsString, MinLength } from "class-validator";
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from "class-validator";
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 
 export class CreateTodoDto {
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
+  @MaxLength(50)
   @ApiProperty()
   title: string;
 
