a43904ae12f6616df6129ae48fbb3a331d20deb1
bflorestal <yayou7710@hotmail.fr>
2024-04-02 10:39:35 +0200

    Ajout de ModeWatcher

--- apps/web/src/App.svelte
+++ apps/web/src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import { ModeWatcher } from "mode-watcher";
   import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
   import TodoContainer from "./components/TodoContainer.svelte";
 
   const queryClient = new QueryClient();
 </script>
 
+<ModeWatcher />
 <QueryClientProvider client={queryClient}>
   <main class="grid">
     <TodoContainer />
