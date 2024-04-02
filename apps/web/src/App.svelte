0be8b2b77a671b1e125e71fc2c0d193046638391
bflorestal <yayou7710@hotmail.fr>
2024-04-02 10:43:41 +0200

    Ajout de svelte-sonner pour les toast

--- apps/web/src/App.svelte
+++ apps/web/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { ModeWatcher } from "mode-watcher";
+  import { Toaster } from "$lib/components/ui/sonner";
   import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
   import TodoContainer from "./components/TodoContainer.svelte";
 
@@ -7,6 +8,7 @@
 </script>
 
 <ModeWatcher />
+<Toaster />
 <QueryClientProvider client={queryClient}>
   <main class="grid">
     <TodoContainer />
