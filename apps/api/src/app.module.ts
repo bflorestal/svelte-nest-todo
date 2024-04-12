843e157c75b086dfa62a424ca62fe3bff4c2136f
bflorestal <yayou7710@hotmail.fr>
2024-04-12 23:35:49 +0200

    Rate limit

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TodoModule } from "./todo/todo.module";
+import { ThrottlerModule } from "@nestjs/throttler";
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TodoModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
